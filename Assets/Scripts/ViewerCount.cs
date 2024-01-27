6634ce14ba0c2294174dff7b7a2af9aaace126a2
SamohtB <75974322+SamohtB@users.noreply.github.com>
2024-01-27 09:13:10 +0800

    Initial View Count

---
+++ Assets/Scripts/ViewerCount.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class ViewerCount : MonoBehaviour
+{
+    [SerializeField] private int currentViewerCount = 500;
+    [SerializeField] private int penalty = 170;
+    [SerializeField] private int reward = 80;
+    [SerializeField] private TextMeshProUGUI viewCountText;
+
+    private void Start()
+    {
+        viewCountText.text = currentViewerCount.ToString();
+    }
+
+    public void ChangeViewerCount(bool isPenalty)
+    {
+        if (isPenalty) 
+        {
+            AddViewerCount(-penalty);
+        }
+        else
+        {
+            AddViewerCount(reward);
+        }
+    }
+
+    private void AddViewerCount(int additionalViewers)
+    {
+        currentViewerCount += additionalViewers;
+        viewCountText.text = currentViewerCount.ToString();
+
+        if(currentViewerCount <= 0)
+        {
+            //Lose Condition
+            EventBroadcaster.Instance.PostEvent(EventNames.ON_GAME_LOSE);
+        }
+    }
+}
