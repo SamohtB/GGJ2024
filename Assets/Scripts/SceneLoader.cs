5dc8ac7557978ddd2cbb208fdd8eb58d367f8a17
hygrintoht <79806551+hygrintoht@users.noreply.github.com>
2024-01-28 02:44:02 +0800

    template main menu and game over screens

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByNumber(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
